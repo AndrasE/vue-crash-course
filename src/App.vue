f7c6c68616576fd4ad7986d6b7b44670e74f9a78
Andras E <rohadtsajt@gmail.com>
2025-04-08 19:14:42 +0100

    v-on, with method

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,31 @@ export default {
   data() {
     return {
       HelloWorld: 'Hello World',
-      status: true,
+      status: 'pending',
       tasks: ["task1", "task2", "task3"],
       link: "https://vuejs.org",
+    };
+  },
+      methods: {
+      toggleStatus() {
+         if (this.status === "active") {
+          this.status = "pending";
+      } else if (this.status === "pending") {
+          this.status = "inactive";
+      } else {
+          this.status = "active";        
+      }
     }
   },
-}
+};
+
 </script>
 
 <template>
   <h1>{{ HelloWorld }}</h1>
-  <p v-if="status">This is a paragraph</p>
-  <p v-else>This is another paragraph</p>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p> 
+  <p v-else>User is inactive</p>
   <ul>
     <li v-for="(task, index) in tasks" :key="index">
       {{ task }}
@@ -25,5 +38,5 @@ export default {
     // or more simply
     <a :href="link">Link 2</a> 
   </p>
-  <button @click="status = !status">Toggle Paragraph</button>
+  <button v-on:click="toggleStatus">Toggle Paragraph</button>
 </template>
