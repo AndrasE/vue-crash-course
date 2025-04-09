10b28c6a27456295cb2f609a588c745afd515abe
Andras E <rohadtsajt@gmail.com>
2025-04-09 07:44:59 +0100

    add tasks, remove tasks
    Form adding new tasks, del button removing tasks

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,53 @@
 <script setup>
 import { ref } from 'vue';
 
-    const HelloWorld = ref('Hello World');
-    let status = ref('pending'); // Changed to let
-    const tasks = ref(["task1", "task2", "task3"]);
+const HelloWorld = ref('Hello World');
+let status = ref('pending'); // Changed to let
+const tasks = ref(["task1", "task2", "task3"]);
+const newTask = ref('');
 
-    const toggleStatus = () => {
-      if (status.value === 'active') {
-        status.value = 'pending';
-      } else if (status.value === 'pending') {
-        status.value = 'inactive';
-      } else {
-        status.value = 'active';
-      }
-    };
+
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = 'inactive';
+  } else {
+    status.value = 'active';
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1); // Remove the task at the specified index Vue reactivity will handle the DOM update
+};
 
 </script>
 
 <template>
   <h1>{{ HelloWorld }}</h1>
   <p v-if="status === 'active'">User is active</p>
-  <p v-else-if="status === 'pending'">User is pending</p> 
+  <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+  <h3>Tasks:</h3>
   <ul>
     <li v-for="(task, index) in tasks" :key="index">
-      {{ task }}
+      <span> {{ task }}
+        <button @click="deleteTask(index)">x</button>
+      </span>
     </li>
   </ul>
+  <form @submit.prevent="addTask">
+    <label for="newTask">New Task:</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Add Task</button>
+  </form>
+  <br />
   <button @click="toggleStatus">Toggle Paragraph</button>
 </template>
