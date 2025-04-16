2f1cbba58e42c1584bdea8b68dd38ab2079ee555
Andras E <rohadtsajt@gmail.com>
2025-04-16 07:35:19 +0100

    dynamic route to individual listing || install json-server

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import JobsView from "../views/JobsVue.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), // create a history object
@@ -15,6 +16,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      path: "/jobs/:id",
+      name: "job",
+      component: JobView,
     },
     {
       path: "/:catchAll(.*)",
