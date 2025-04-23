6e05bc947ed964a2991bd019ba5952ae2ceab9cc
Andras E <rohadtsajt@gmail.com>
2025-04-23 06:49:02 +0100

    back button

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { reactive, onMounted } from 'vue';
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
-import { useRoute } from 'vue-router';
+import BackButton from '@/components/BackButton.vue';
+import { useRoute, RouterLink } from 'vue-router';
 import axios from 'axios';
 
 const route = useRoute();
@@ -45,6 +46,7 @@ onMounted(async () => {
 </script>
 
 <template>
+    <BackButton />
     <section v-if="!state.isLoading" class="bg-green-50">
         <div class="container m-auto py-10 px-6">
             <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
@@ -91,9 +93,10 @@ onMounted(async () => {
                     <!-- Manage -->
                     <div class="bg-white p-6 rounded-lg shadow-md mt-6">
                         <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-                        <a href="add-job.html"
-                            class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">Edit
-                            Job</a>
+                        <RouterLink :to="`/jobs/edit/${state.job.id}`"
+                            class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                            Edit
+                            Job</RouterLink>
                         <button
                             class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                             Delete Job
